d9bb154872d2fa682a28dcdb46a3d0fa864ff66e
DamianoCelesti <damiano.celesti@gmail.com>
2025-01-13 15:27:29 +0100

    added if

--- script.js
+++ script.js
@@ -7,8 +7,16 @@ const bottone = document.getElementById("bottoneLamp");
 
 // evento
 bottone.addEventListener("click", function () {
-    // cambio dell img
-    lampadaAccesa.src = "./img/yellow_lamp.png"
-    // cambio del testo nel bottone
-    bottone.textContent = "SPEGNI"
-})
+    // cambio dell img   
+    if (lampadaAccesa.src.includes("yellow_lamp.png")) {
+        lampadaAccesa.src = "./ig/white_lamp.png";
+        lampadaAccesa.alt = "Lampada spenta";
+        bottone.textContent = "ACCENDI";
+    } else {
+        // cambio del testo nel bottone
+        lampadaAccesa.src = "./im/yellow_lamp.png";
+        lampadaAccesa.alt = "Lampada accesa";
+        bottone.textContent = "SPEGNI";
+    }
+    
+});
